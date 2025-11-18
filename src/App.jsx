d5811c9921b16eb65bc4fcc8cd0a534d6f07bf95
Miguel García Durán <miguel@estanoche.es>
2025-11-19 00:46:53 +0100

    final: landing 100% perfecta – marca de agua zambombas, simetría total, todo centrado y profesional

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,25 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-
-// Páginas principales
-import AltaUsuario from './pages/altausuario.jsx'
-import CrearLocalOGrupo from './pages/crearlocalogrupo.jsx'
-import Agenda from './pages/agenda.jsx'
-// import FichaEvento from './pages/ficha_evento.jsx'   ← comentada o borrada
-
-// Página genérica para legales (sobre, legal, privacidad, cookies, términos)
-import StaticPage from './pages/StaticPage.jsx'
+# Corregir el import en app.jsx (una sola línea)
+@'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AltaUsuario from "./pages/AltaUsuario.jsx";
+import CrearLocalOGrupo from "./pages/CrearLocalOGrupo.jsx";
+import Agenda from "./pages/Agenda.jsx";
+import StaticPage from "./pages/StaticPage.jsx";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* Rutas principales */}
         <Route path="/" element={<AltaUsuario />} />
         <Route path="/crear_local_o_grupo" element={<CrearLocalOGrupo />} />
         <Route path="/agenda" element={<Agenda />} />
-
-        {/* Rutas que vienen de los .html (gracias a vercel.json) */}
-        <Route path="/alta-usuario" element={<AltaUsuario />} />
-        <Route path="/evento" element={<FichaEvento />} />
-        <Route path="/perfil" element={<AltaUsuario />} />          {/* temporal hasta tener ficha_usuario */}
-        <Route path="/registro" element={<AltaUsuario />} />       {/* temporal hasta tener registro real */}
-
-        {/* Páginas legales y estáticas */}
-        <Route path="/sobre" element={<StaticPage />} />
+        <Route path="/sobre" element {<StaticPage />} />
         <Route path="/legal" element={<StaticPage />} />
         <Route path="/privacidad" element={<StaticPage />} />
         <Route path="/cookies" element={<StaticPage />} />
-        <Route path="/terminos" element={<StaticPage />} />
+        <Route path="/terminos"  element={<StaticPage />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
+'@ > src/app.jsx
