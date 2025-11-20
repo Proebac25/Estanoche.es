c94816d29823bf1991b33f62448462fb4c18f449
Miguel García Durán <miguel@estanoche.es>
2025-11-20 06:47:07 +0100

    Gemini vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,18 @@
-﻿import { defineConfig } from 'vite'
+﻿// vite.config.js
+
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // 🔑 ESTA ES LA CLAVE PARA CORREGIR LA CARGA EN VERCEL
+  base: './', 
   build: {
     rollupOptions: {
       input: './index.html'
     },
-    assetsInlineLimit: 0,  // No inlinizar nada
+    assetsInlineLimit: 0,
     emptyOutDir: true,
     copyPublicDir: true
   },
